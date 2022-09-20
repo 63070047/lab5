7df99df52447149fcc6234fd25ec6c546a6948ab
Asuka81 <bakaboy001@gmail.com>
2022-09-20 10:30:06 +0700

    go

--- src/main/java/com/example/lab5/MyView2.java
+++ src/main/java/com/example/lab5/MyView2.java
@@ -43,6 +43,8 @@ public class MyView2 extends HorizontalLayout {
         v2.add(addSence, btnAddSentence, showGoodSence, showBadSence, btnShowSentence);
         add(v1,v2);
 
+        goodWordBox.setItems(words.goodWords);
+        badWordBox.setItems(words.badWords);
 
         btnGoodWord.addClickListener(event -> {
             String word = addWord.getValue();
